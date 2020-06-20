752aefe94d4ea1ba8d6f1ccd1cb33c418b3fe197
Raksha Marskole <65505713+rakshamarskole@users.noreply.github.com>
2020-06-20 17:26:22 +0530

    This file checks for eligibility for voting.
    This file is created to check whether the person is eligible for voting or not. In this file If-else statement is used.

---
+++ vote check.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int age;
+ cout<<"Enter your age: ";
+ cin>>age;
+ 
+ if(age>=18)
+ {
+ 	cout<<"You are ELIGIBLE for voting\a ";
+ }	
+ else
+ {
+ 	cout<<"You are NOT ELIGIBLE for voting\a\a\a";
+ }
+ return 0;
+}
